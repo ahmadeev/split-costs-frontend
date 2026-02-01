75c3e9d768ecd0b9d0b4e1d8af63fcd271f0ff87
danis <ahrnadeev@gmail.com>
2026-02-01 23:26:48 +0300

    feat: add pagination

--- src/ui/Table/Pagination/Pagination.tsx
+++ src/ui/Table/Pagination/Pagination.tsx
@@ -19,17 +19,18 @@ export default function Pagination({ currentPage, pageCount, delta, onChange }: 
         onChange(page);
     }
 
-    function getLink(page: number, isCurrentPage: boolean) {
+    function getLink(page: number, isCurrentPage: boolean, key: number) {
         return (
             <Button
                 type={'link'}
                 title={isCurrentPage ? `[ ${String(page)} ]` : String(page)}
                 onClick={() => { handlePageChange(page); }}
+                key={key}
             />
         );
     }
 
-    const getPagesArray = (currentPage: number, pageCount: number, delta: number) => {
+    const getPageNumbersArray = (currentPage: number, pageCount: number, delta: number) => {
         return [1, ...range(currentPage - delta, currentPage + delta), pageCount]
             .reduce((acc: number[], v: number, i: number) => {
                 if ((i === 0 || v > acc[acc.length - 1]) && (v >= 1 && v <= pageCount)) {
@@ -38,6 +39,20 @@ export default function Pagination({ currentPage, pageCount, delta, onChange }: 
 
                 return acc;
             }, []);
+    };
+
+    const getPagesArray = (currentPage: number, pageCount: number, delta: number) => {
+        const pages: (number | '...')[] = [];
+
+        getPageNumbersArray(currentPage, pageCount, delta).forEach((page: number, index: number, arr: number[]) => {
+            if (index && page - arr[index - 1] > 1) {
+                pages.push('...');
+            }
+
+            pages.push(page);
+        });
+
+        return pages;
     };
 
     const pages = getPagesArray(currentPage, pageCount, delta);
@@ -59,27 +74,10 @@ export default function Pagination({ currentPage, pageCount, delta, onChange }: 
                 isDisabled={currentPage === pages[0]}
             />
             {
-                pages.map((page: number, index: number, arr: number[]) => {
-                    const isCurrentPage = currentPage === page;
-
-                    if (index === 0) {
-                        return getLink(page, isCurrentPage);
-                    }
-
-                    if (page - arr[index - 1] > 1) {
-                        return (
-                            <>
-                                <span>...</span>
-                                { getLink(page, isCurrentPage) }
-                            </>
-                        );
-                    }
-
-                    return (
-                        <>
-                            { getLink(page, isCurrentPage) }
-                        </>
-                    );
+                pages.map((item: number | '...', index: number) => {
+                    return item === '...' ? (
+                        <span key={index}>...</span>
+                    ) : getLink(item, item === currentPage, index);
                 })
             }
             <Button
