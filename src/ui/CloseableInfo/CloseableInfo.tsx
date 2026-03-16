9a616edae2d9319d130a02258cccaf3c52ef7398
danis <ahrnadeev@gmail.com>
2026-03-16 21:26:58 +0300

    feat: add closeable info

--- src/ui/CloseableInfo/CloseableInfo.tsx
+++ src/ui/CloseableInfo/CloseableInfo.tsx
@@ -1,18 +1,31 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useState } from 'react';
 import Close from '../../icons/close_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg?react';
 
 export default function CloseableInfo({ children }: { children?: ReactNode }) {
+    const [open, setOpen] = useState(true);
+
+    if (!open) return null;
+
     return (
         <div
             style={{
                 backgroundColor: 'var(--third-background-color)',
-                padding: '1rem 2rem',
+                padding: '2rem',
                 width: 'fit-content',
                 borderRadius: 'var(--border-radius)',
                 position: 'relative',
             }}
         >
-            <Close style={{ position: 'absolute', top: '1rem', right: '1rem', fill: 'var(--color)' }}></Close>
+            <Close
+                style={{
+                    position: 'absolute',
+                    top: '1rem',
+                    right: '1rem',
+                    fill: 'var(--color)',
+                    cursor: 'pointer',
+                }}
+                onClick={() => { setOpen(false); }}
+            />
             {children}
         </div>
     );
